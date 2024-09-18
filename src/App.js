30bd5828e389f7a8f9902a483a32f48070c4440b
tcantunes <103459242+tcantunes@users.noreply.github.com>
2024-09-18 13:38:37 -0300

    feat: Adiciona página de cursos disponíveis

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Cursos from './pages/Cursos';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/cursos" element={<Cursos />} />
       </Routes>
     </Router>
   );
