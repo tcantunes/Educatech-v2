0469ae4d1f0da9f7c9d93f2d39fd14b35acb6b9c
tcantunes <103459242+tcantunes@users.noreply.github.com>
2024-10-02 17:11:48 -0300

    add feature

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Cursos from './pages/Cursos';
 import VideoPage from './pages/Videos';
+import UserProfile from './pages/PaginaUsuario';
+import Contact from './pages/Contato';
+import VerifyLogin from './pages/VerifyLogin'; 
+import ProtectedRoute from './components/ProtectedRoute';
+import PartnersPage from './pages/Parceiros';
 
 const App = () => {
   return (
@@ -13,8 +18,33 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/cursos" element={<Cursos />} />
-        <Route path="/videos/:curso" element={<VideoPage />} />
+        <Route path="/verify-login" element={<VerifyLogin />} />
+        <Route
+          path="/cursos"
+          element={
+            <ProtectedRoute>
+              <Cursos />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <UserProfile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/videos/:curso"
+          element={
+            <ProtectedRoute>
+              <VideoPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/parceiros" element={<PartnersPage />} />
       </Routes>
     </Router>
   );
